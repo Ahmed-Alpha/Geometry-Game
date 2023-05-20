825f17debe1c0526fd9a165865c93cc88e58d833
Ahmed Alfadhel <133564205+Ahmed-Alpha@users.noreply.github.com>
2023-05-20 11:30:25 +0100

    Add files via upload

---
+++ game.py
@@ -0,0 +1,18 @@
+import turtle
+
+
+myturtle = turtle.Turtle()
+
+myturtle.penup()
+myturtle.goto(54,75)#Start at that coordinate
+
+myturtle.pendown()
+myturtle.forward(100)# Moves 100(amount) of pixels
+myturtle.left(90)# Turns specific angle
+myturtle.forward(200)
+myturtle.left(90)
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(200)
+
+turtle.done()
